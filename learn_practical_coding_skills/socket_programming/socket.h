a84354b0316fb6bab539d2740faa4641dd7a78e7
jyjeon <drtkdldjstm@naver.com>
2022-10-21 09:23:19 +0900

    update socket_programming

---
+++ learn_practical_coding_skills/socket_programming/socket.h
@@ -0,0 +1,14 @@
+#ifndef SOCKET_H
+#define SOCKET_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+void error_handling(char *message);
+
+
+#endif
