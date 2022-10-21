3511dc62a1d3024677f4daa8b7859efce5769c58
jyjeon <drtkdldjstm@naver.com>
2022-10-21 13:05:28 +0900

    update socket

--- learn_practical_coding_skills/socket_programming/main_server.c
+++ learn_practical_coding_skills/socket_programming/main_server.c
@@ -9,6 +9,7 @@ int main(int argc, char *argv[]){
 	socklen_t clnt_addr_size;
 
 	char message[] = "Hello World!";
+	int str_len; 
 
 	if(argc != 2){
 		printf("Usage : %s <port>\n", argv[0]);
@@ -31,12 +32,26 @@ int main(int argc, char *argv[]){
 		error_handling("listen() error");
 
 	clnt_addr_size = sizeof(clnt_addr);
-	clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
-	if(clnt_sock==-1)
-		error_handling("accept() error");
 
-	write(clnt_sock, message, sizeof(message));
+	while(1){
+	clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+	printf("client address : %s\n",inet_ntoa(clnt_addr.sin_addr)); // print ip
+	
+	if(clnt_sock == -1)
+		error_handling("accept() error");
+	else                       
+		printf("Connected client\n");
+
+	while((str_len = read(clnt_sock, message, BUF_SIZE)) != 0)
+		write(clnt_sock, message, str_len);
+	
+	if(strcmp(message, "q"))
+		break;
+	printf("Message from client : %s\n", message);
+
 	close(clnt_sock);
+	}
+
 	close(serv_sock);
 
 	return 0;
