727c56112efb43e4eaab505ecdd3614769aa1254
JeonJoonyong <drtkdldjstm@naver.com>
2023-05-30 23:55:43 +0900

    update 두 수의 염산값 비교하기

--- C_알고리즘_문제/프로그래머스/Lv0/두_수의_연산값_비교하기.c
+++ C_알고리즘_문제/프로그래머스/Lv0/두_수의_연산값_비교하기.c
@@ -6,21 +6,12 @@ int solution(int a, int b) {
     int answer = 0;
     char buffer[16] = {0, };
     int temp;
-    //snprintf(buffer, sizeof(buffer), "%c%c\0", a, b);  
-    //printf("%s\n", buffer);
-    if (a < 10)
-        printf("%c\n", a+'0');
-    else if (a / 10 < 10)
-        snprintf(buffer, sizeof(buffer), "%c", a/10);
-    else {
-        temp = a / 10;
-        while (temp < 10) {
-            temp = temp / 10;
-        }
-        snprintf(buffer, sizeof(buffer), "%c", a/10);
-    }
+
+    snprintf(buffer, sizeof(buffer), "%d%d", a, b);
+    temp = atoi(buffer);
     
-    printf("%s\n", buffer);
+    if (temp > 2*a*b) answer = temp;
+    else answer = 2*a*b;
     
     return answer;
 }
