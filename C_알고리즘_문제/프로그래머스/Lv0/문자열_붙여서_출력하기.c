68111876320e00a540d7baba104c9a53f8e364dc
JeonJoonyong <drtkdldjstm@naver.com>
2023-06-08 23:11:57 +0900

    update 문자열 붙여서 출력하기 : Lv0

---
+++ C_알고리즘_문제/프로그래머스/Lv0/문자열_붙여서_출력하기.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#define LEN_INPUT1 11
+#define LEN_INPUT2 11
+
+int main(void) {
+    char s1[LEN_INPUT1];
+    char s2[LEN_INPUT2];
+    char answer[22];
+    scanf("%s %s", s1, s2);
+    
+    strcpy(answer, s1);
+    strcat(answer, s2);
+    printf("%s", answer);
+
+    return 0;
+}
